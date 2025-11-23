792085fb933687619bf85e8e8d4b87c888fcc483
Diritas42 <s.d.trabo@students.psu.by>
2025-11-23 21:58:53 +0300

    Update Player.js

--- js/modules/Player.js
+++ js/modules/Player.js
@@ -98,9 +98,10 @@ class Player {
     /**
      * Устранение врага
      * @param {Array} enemies - массив врагов
+     * @param {boolean} isHighAlertMode - режим повышенной готовности
      */
-    eliminateEnemy(enemies) {
-        if (this.eliminationCooldown > 0) return;
+    eliminateEnemy(enemies, isHighAlertMode) {
+        if (this.eliminationCooldown > 0 || isHighAlertMode) return;
         
         for (const enemy of enemies) {
             if (!enemy.isEliminated && enemy.canBeEliminated(this.x, this.y, this.direction)) {
