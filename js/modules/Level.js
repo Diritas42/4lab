2df5292b8663968ae6d506a68bbf0f5787a1a00d
Diritas42 <s.d.trabo@students.psu.by>
2025-11-24 11:02:53 +0300

    Update Level.js

--- js/modules/Level.js
+++ js/modules/Level.js
@@ -23,13 +23,17 @@ class Level {
             ctx.fillRect(wall[0], wall[1], wall[2], wall[3]);
         });
         
-        // Отрисовка выхода (центрированный)
+        // Отрисовка выхода - исправлено смещение
+        const exitSize = 20;
+        const exitX = this.exit[0] - exitSize/2;
+        const exitY = this.exit[1] - exitSize/2;
+        
         ctx.fillStyle = '#2196F3';
-        ctx.fillRect(this.exit[0] - 15, this.exit[1] - 15, 30, 30);
+        ctx.fillRect(exitX, exitY, exitSize, exitSize);
         ctx.fillStyle = '#fff';
         ctx.font = '12px Arial';
         ctx.textAlign = 'center';
-        ctx.fillText('ВЫХОД', this.exit[0], this.exit[1] - 20);
+        ctx.fillText('ВЫХОД', this.exit[0], this.exit[1] - 15);
     }
 }
 
